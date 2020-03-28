5e81f9570df6e9c4e268437ca4bab2746ba2cf62
Taazim Temirova <tezi.temirova@gmail.com>
2020-03-28 12:51:39 +0600

    renamed application name

--- java_exam_7_taazim_temirova/src/main/java/kg/attractor/orders/OrdersApplication.java
+++ java_exam_7_taazim_temirova/src/main/java/kg/attractor/orders/OrdersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaazimTemirovaApplication {
+public class OrdersApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TaazimTemirovaApplication.class, args);
+        SpringApplication.run(OrdersApplication.class, args);
     }
 
 }
